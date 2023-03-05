66dfe27cdafedd77030444960dfd26d07a7b214d
devklick <devklick@gmail.com>
2023-03-05 14:45:54 +0000

    default values

--- src/BasicPoker.Core/Cards/CardMetadata.cs
+++ src/BasicPoker.Core/Cards/CardMetadata.cs
@@ -6,7 +6,7 @@ public class CardMetadata
     public CardSuit Suit { get; set; }
     public CardRank Rank { get; set; }
     public CardSuitColor Color { get; set; }
-    public int[] Values { get; set; }
-    public string SuitSprite { get; set; }
-    public string RankSprite { get; set; }
+    public int[] Values { get; set; } = Array.Empty<int>();
+    public string SuitSprite { get; set; } = string.Empty;
+    public string RankSprite { get; set; } = string.Empty;
 }
